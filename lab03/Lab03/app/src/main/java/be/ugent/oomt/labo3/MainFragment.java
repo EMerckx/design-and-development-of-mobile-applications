fae395e7f607b071c08dd307ab16cca8d7856319
EMerckx <ewoutmerckx.pro@gmail.com>
2015-10-12 16:47:10 +0200

    end of lab, but not finished

--- lab03/Lab03/app/src/main/java/be/ugent/oomt/labo3/MainFragment.java
+++ lab03/Lab03/app/src/main/java/be/ugent/oomt/labo3/MainFragment.java
@@ -2,12 +2,15 @@ package be.ugent.oomt.labo3;
 
 
 import android.app.ListFragment;
+import android.app.LoaderManager;
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 
 /**
  * Created by elias on 12/01/15.
@@ -22,12 +25,32 @@ public class MainFragment extends ListFragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        final int ID = 0;
         // TODO: initialize asynchronous loader
+        getLoaderManager().initLoader(ID, null, null);
+
+        // CREATE THE ADAPTER USING THE CURSOR POINTING TO THE DESIRED DATA AS WELL AS THE LAYOUT INFORMATION
+        SimpleCursorAdapter mAdapter = new SimpleCursorAdapter(getActivity(),
+                android.R.layout.simple_list_item_activated_2, null,
+                new String[] { ContactsContract.Contacts.DISPLAY_NAME, ContactsContract.Contacts.CONTACT_STATUS },
+                new int[] { android.R.id.text1, android.R.id.text2 }, 0);
+
+
 
         // TODO: Change ArrayAdapter to SimpleCursorAdapter to access the ContentProvider
         ListAdapter listAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_activated_1,
                 getResources().getStringArray(R.array.superheroes_names));
         setListAdapter(listAdapter);
+
+/*
+        new CursorLoader(
+                getActivity(),   // Parent activity context
+                mDataUrl,        // Table to query
+                mProjection,     // Projection to return
+                null,            // No selection clause
+                null,            // No selection arguments
+                null             // Default sort order
+        );*/
     }
 
     @Override
